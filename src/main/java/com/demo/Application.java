eb6ec21029e57a988de1c2032152427a085511e4
Elliott <william.e.hamilton125.mil@swf.army.mil>
2021-08-19 13:35:46 -0500

    more messing around

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -2,6 +2,8 @@ package com.demo;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length > 0){
+            System.out.println("hi " + args[0]);
+        }
     }
 }
